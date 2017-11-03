5e4214df691409c648d35e9a2c168445b9fd8311
swatantra7 <swatantrajha7@gmail.com>
2017-11-04 00:20:24 +0530

    This commit refs - schedular changes

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,5 @@
 set :output, {:error => 'error.log', :standard => 'cron.log'}
-every 1.minute do # 1.minute 1.day 1.week 1.month 1.year is also supported
+
+every 1.day, at: '12:00 am' do
   rake "generate_certificate:generate_data"
 end
